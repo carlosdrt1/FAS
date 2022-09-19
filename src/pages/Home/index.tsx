ce2f287bec69e73921a0eb852bf8393f1fec0064
carlosdrt1 <carlosdrt229@gmail.com>
2022-09-19 11:05:18 -0300

    Fale conosco e mapa

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import "./styles.css";
 import card1 from "../../assets/images/card1.svg";
 import card2 from "../../assets/images/card2.svg";
 import card3 from "../../assets/images/card3.svg";
+import imagem from "../../assets/images/faleConosco.svg";
 import { Link } from "react-router-dom";
 
 const Home = () => {
@@ -27,7 +28,7 @@ const Home = () => {
         <div className="container card-container">
           <div className="row">
             <div className="col-lg-4">
-              <div className="card position-static bg-light border-dark">
+              <div className="card position-static bg-light shadow">
                 <img src={card1} alt="" className="card-img-top" />
                 <div className="card-body text-center">
                   <h4 className="card-tittle">Acessoria empresarial</h4>
@@ -42,7 +43,7 @@ const Home = () => {
               </div>
             </div>
             <div className="col-lg-4">
-              <div className="card position-static bg-light border-warning">
+              <div className="card position-static bg-light shadow">
                 <img src={card2} alt="" className="card-img-top" />
                 <div className="card-body text-center">
                   <h4 className="card-tittle">Certificado Digital</h4>
@@ -58,7 +59,7 @@ const Home = () => {
               </div>
             </div>
             <div className="col-lg-4">
-              <div className="card position-static bg-light">
+              <div className="card position-static bg-light shadow">
                 <img src={card3} alt="" className="card-img-top" />
                 <div className="card-body text-center">
                   <h4 className="card-tittle">
@@ -77,6 +78,31 @@ const Home = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="container container-fluid fale-conosco">
+        <div className="row">
+          <div className="col-lg-6 text">
+            <h4 className="tittle-h3">
+              O mundo está mais dinâmico, e sua contabilidade, também?
+            </h4>
+            <p>
+              Todas as decisões influenciam nos lucros da sua empresa. Vamos
+              conversar sobre como promover mudanças positivas nela. Será um
+              prazer atender você.
+            </p>
+            <button className="btn">Fale conosco</button>
+          </div>
+          <img src={imagem} alt="" className="col-lg-6 collapse-md" />
+        </div>
+      </div>
+      <div className="container-fluid map-container">
+        <iframe
+          width="100%"
+          height="300px"
+          title="mapa"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.278953287401!2d-39.45452918569084!3d-6.09307596146696!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7a2d229c91ad8c3%3A0x8e7ec802c8d67873!2sAv.%20Jos%C3%A9%20Marques%20Filho%2C%2097%20-%20Centro%2C%20Acopiara%20-%20CE%2C%2063560-000!5e0!3m2!1spt-BR!2sbr!4v1663595795330!5m2!1spt-BR!2sbr"
+          loading={"lazy"}
+        ></iframe>
       </div>
     </>
   );
